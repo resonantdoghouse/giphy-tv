2640448e45ab7d84738e6c4eccc4d8f3fd99aafb
Jim <jim@advancebass.com>
2022-03-31 06:12:26 -0700

    start of giphy tv app

--- src/App.js
+++ src/App.js
@@ -5,11 +5,32 @@ import './App.css';
 function App() {
   const [gif, setGif] = useState(null);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    getRandomGif();
+  }, []);
+
+  const getRandomGif = () => {
+    axios
+      .get(
+        `https://api.giphy.com/v1/gifs/random?rating=g&api_key=${'CzT2T3MPsJggrpCiPFitxUC1Gxp7XAkp'}`
+      )
+      .then((response) => {
+        setGif(response.data.data.images.original.url);
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className="App">
       <h1>Giphy TV</h1>
+      {gif && (
+        <img
+          onClick={getRandomGif}
+          className="gif"
+          src={gif}
+          alt="giphy random"
+        />
+      )}
     </div>
   );
 }
